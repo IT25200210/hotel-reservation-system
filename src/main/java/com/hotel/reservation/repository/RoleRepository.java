564f577bbd62a94f36ac4e7267ac13afb34a8626
it25104131 <it25104131@my.sliit.lk>
2026-08-24 21:30:27 +0530

    created Role and User repositories

---
+++ src/main/java/com/hotel/reservation/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.hotel.reservation.repository;
+import com.hotel.reservation.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
