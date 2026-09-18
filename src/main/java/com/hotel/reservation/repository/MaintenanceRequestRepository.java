90a63d91c5bd1fde8f941b88365230fc0ffe9454
IT25200131 <it25200131@my.sliit.lk>
2026-09-18 21:15:51 +0530

    Delete src/main/java/com/hotel/reservation/repository/MaintenanceRequestRepository.java

--- src/main/java/com/hotel/reservation/repository/MaintenanceRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.reservation.repository;
-
-import com.hotel.reservation.entity.MaintenanceRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface MaintenanceRequestRepository extends JpaRepository<MaintenanceRequest, Long> {
-    List<MaintenanceRequest> findByStatus(String status);
-}
