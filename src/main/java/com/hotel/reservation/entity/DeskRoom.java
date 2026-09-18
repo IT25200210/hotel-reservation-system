a732221b015cccd127099885fc9331eab5d1d232
Nethara Rathnaweera <it25200131@my.sliit.lk>
2026-09-18 17:48:44 +0530

    Create DeskRoom.java
    Add DeskRoom repository

---
+++ src/main/java/com/hotel/reservation/entity/DeskRoom.java
@@ -0,0 +1,40 @@
+package com.hotel.reservation.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "desk_rooms")
+public class DeskRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String number;
+
+    @Column(nullable = false)
+    private boolean occupied;
+
+    protected DeskRoom() {}
+
+    public DeskRoom(String number) {
+        this.number = number;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
+}
